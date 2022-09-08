61f794ad7b9698a4f3fa020f38fff006fbfea600
maxembo <91334358+maxembo@users.noreply.github.com>
2022-09-08 12:48:30 +0300

    ready PracticalWork14.4

---
+++ PracticalWork14.4/PracticalWork14.4/PracticalWork14.4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	std::string name = "Maxim Popov";
+
+	std::cout<< "Строка: " << name;
+
+	std::cout << "\nДлина строки: " << name.length();
+
+	std::cout << "\nПервой символ: " << name.front();
+
+	std::cout << "\nПоследний символ: " << name.back();
+
+	return 0;
+}
+
